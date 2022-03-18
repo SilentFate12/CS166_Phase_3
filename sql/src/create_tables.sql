19759e8b24380cf3571b3ddfa136809ecbfd9d61
Cody Phu <codyphu168@yahoo.com>
2022-03-18 02:23:45 -0700

    Update create_tables.sql
    fixed minor errors and changed userId to char(30) because some names were too big

--- sql/src/create_tables.sql
+++ sql/src/create_tables.sql
@@ -6,15 +6,15 @@ DROP TABLE IF EXISTS USR CASCADE;
 
 
 CREATE TABLE USR(
-	userId varchar(10) UNIQUE NOT NULL, 
-	password varchar(10) NOT NULL,
+	userId varchar(30) UNIQUE NOT NULL, 
+	password varchar(30) NOT NULL,
 	email text NOT NULL,
 	name char(50),
 	dateOfBirth date,
 	Primary Key(userId));
 
 CREATE TABLE WORK_EXPR(
-	userId char(10) NOT NULL, 
+	userId char(30) NOT NULL, 
 	company char(50) NOT NULL, 
 	role char(50) NOT NULL,
 	location char(50),
@@ -26,7 +26,7 @@ CREATE TABLE WORK_EXPR(
 	);
 
 CREATE TABLE EDUCATIONAL_DETAILS(
-	userId char(10) NOT NULL, 
+	userId char(30) NOT NULL, 
 	instituitionName char(50) NOT NULL, 
 	major char(50) NOT NULL,
 	degree char(50) NOT NULL,
@@ -39,8 +39,8 @@ CREATE TABLE EDUCATIONAL_DETAILS(
 
 CREATE TABLE MESSAGE(
 	msgId integer UNIQUE NOT NULL, 
-	senderId char(10) NOT NULL,
-	receiverId char(10) NOT NULL,
+	senderId char(30) NOT NULL,
+	receiverId char(30) NOT NULL,
 	contents char(500) NOT NULL,
 	sendTime timestamp,
 	deleteStatus integer,
@@ -53,26 +53,10 @@ CREATE TABLE MESSAGE(
 );
 
 CREATE TABLE CONNECTION_USR(
-	userId char(10) NOT NULL, 
+	userId char(30) NOT NULL, 
 	connectionId char(10) NOT NULL, 
 	status char(30) NOT NULL,
 	PRIMARY KEY(userId,connectionId),
 	FOREIGN KEY(userId) 
 		REFERENCES User(userId)
 );
-
-CREATE TABLE MESSAGE(
-	msgId integer UNIQUE NOT NULL, 
-	senderId char(10) NOT NULL,
-	receiverId char(10) NOT NULL,
-	contents char(500) NOT NULL,
-	sendTime timestamp,
-	deleteStatus integer,
-	status char(30) NOT NULL,
-	PRIMARY KEY(msgId));
-
-CREATE TABLE CONNECTION_USR(
-	userId char(10) NOT NULL, 
-	connectionId char(10) NOT NULL, 
-	status char(30) NOT NULL,
-	PRIMARY KEY(userId,connectionId));
